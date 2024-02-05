18f58117df61239006c22534e80dde715c735040
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2024-02-05 07:38:40 -0300

    modificando script

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,7 +6,7 @@ let audio = new Audio();
 let mappedKeys = [];
 
 const playTune = (key) => {
-    audio.src = `../src/tunes/${key}.wav`
+    audio.src = `/src/tunes/${key}.wav`
     audio.play();
     const clickedKey = document.querySelector(`[data-key="${key}"]`);
     clickedKey.classList.add("active");
